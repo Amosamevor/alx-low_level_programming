1ea8d4036eb4e4bab6a588b4adec64f08ce5b19f
Amosamevor <amosamevor@gmail.com>
2023-03-21 20:11:50 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function prints
+ *
+ * Return:0
+ */
+
+void print_alphabet_x10(void);
+{
+int i;
+char j;
+for (i = 1 ; j <= 10 ; i++)
+{
+for (j = 'a' ; i <= 'z' ; j++)
+_putchar(j);
+_putchar('\n');
+}
+
+}
