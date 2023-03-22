0bbd8c33f8e81630ac10ef4fb5ba725fb03e4902
Amosamevor <amosamevor@gmail.com>
2023-03-22 00:29:51 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+
+/**
+ * print_to_98 - function prints
+ * @n: parameter
+ *
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n == 98)
+	{
+		printf("98");
+	}
+	else if (n < 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+				printf(", ");
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n ; i >= 98 ; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+				printf(", ");
+
+		}
+	}
+	printf("\n");
+}
+
