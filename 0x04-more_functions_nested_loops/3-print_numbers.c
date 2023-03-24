f1b7e45d1a53bd9d407ab302bde14ccecf07789c
Amosamevor <amosamevor@gmail.com>
2023-03-23 17:08:04 -0700

    task 3 and 6 commit...

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
+
 /**
- * print_most_numbers - functions
- * (neither 2 nor 4).
+ * print_numbers - prints numbers between 0 to 9.
  * Return: no return.
  */
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	int ch;
 
 	for (ch = 48; ch < 58; ch++)
 	{
-		if (ch != 50 && ch != 52)
-			_putchar(ch);
+		_putchar(ch);
 	}
 	_putchar('\n');
 }
