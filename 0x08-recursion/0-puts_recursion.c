570820fd5a83751b75b37239680787ed64b96421
Amosamevor <amosamevor@gmail.com>
2023-04-06 14:27:09 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string, followed by a (\n)
+ *
+ * @s: string
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
