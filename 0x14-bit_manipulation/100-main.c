e09397dcee5734bb97f0f935b7dd3da874541165
Amosamevor <amosamevor@gmail.com>
2023-05-04 11:31:58 +0000

    waitting for checker

---
+++ 0x14-bit_manipulation/100-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * main - function to check code
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int n;
+
+n = get_endianness();
+if (n != 0)
+{
+printf("Little Endian\n");
+}
+else
+{
+printf("Big Endian\n");
+}
+return (0);
+}
